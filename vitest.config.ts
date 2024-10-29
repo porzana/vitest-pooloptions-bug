b0077687f48a007631faacadc18a08c234391635
porzana <117564699+porzana@users.noreply.github.com>
2024-10-29 11:13:15 +0100

    Initial commit

---
+++ vitest.config.ts
@@ -0,0 +1,30 @@
+// vitest.config.ts
+import { defineConfig } from "vitest/config";
+
+const execArgv = [
+	"--this-option Does Not Exist so it should throw an error :(",
+];
+
+export default defineConfig({
+	test: {
+		poolOptions: {
+			forks: {
+				execArgv,
+				minForks: 100000000000,
+			},
+			threads: {
+				execArgv,
+				minThreads: 100000000000,
+			},
+			vmForks: {
+				execArgv,
+				minForks: 100000000000,
+			},
+			vmThreads: {
+				execArgv,
+				minThreads: 100000000000,
+			},
+		},
+		include: ["**/*.test.ts"],
+	},
+});
